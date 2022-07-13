94e00410fcc6c8e2031e32fe8d7b2f5ec293b2b8
Kevin Reid <kevinnivek@me.com>
2022-07-13 13:16:29 -0400

    created server

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
 
-// Use apiRoutes
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+const {notes } = require('./db/db.json');
+
+app.get('/api/notes', (req, res) => {
+  res.send("What's up Buttercup?!")
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
