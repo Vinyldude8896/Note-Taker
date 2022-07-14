d05082af670c7b2977cc641c04556f41fa45a778
Kevin Reid <kevinnivek@me.com>
2022-07-14 07:09:57 -0400

    save note working

--- Routes/Api_routes/notes.js
+++ Routes/Api_routes/notes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const {notes} = require('../../db/db.json');
+const { notes } = require('../../db/db.json');
+const { createNewNote } = require('../../lib/notes');
+
 
 router.get('/api/notes', (req, res) => {
 
     let result = notes;
+    console.log("notes", result)
     if (result) {
         res.json(result);
     } else {
@@ -11,5 +14,12 @@ router.get('/api/notes', (req, res) => {
         res.send(404);
     } 
 })
+
+
+router.post('/api/notes', (req, res) => {
+    const note = createNewNote(req.body, notes);
+    console.log(note);
+    res.json(note);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const htmlRoutes = require('./Routes/Api_routes')
+const htmlRoutes = require('./Routes/HTML_routes')
 const notesRoutes = require('./Routes/Api_routes')
 
-app.use(express.urlencoded({ extended: true}));
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
 app.use(express.json());
+
 app.use(express.static('public'));
 
 // const {notes} = require('./db/db.json');
@@ -19,7 +23,8 @@ app.use(express.static('public'));
 // USe api routes
 
 app.use('/', htmlRoutes);
-app.use('/api/notes', notesRoutes);
+app.use(notesRoutes);
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
